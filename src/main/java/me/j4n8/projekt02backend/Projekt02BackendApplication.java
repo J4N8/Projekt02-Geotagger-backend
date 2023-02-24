4f4b1e3f5a0272213a0d0e1dc1091713db100f47
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-02-24 11:19:36 +0100

    Initial commit

---
+++ src/main/java/me/j4n8/projekt02backend/Projekt02BackendApplication.java
@@ -0,0 +1,14 @@
+package me.j4n8.projekt02backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Projekt02BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt02BackendApplication.class, args);
+    }
+}
---
+++ src/test/java/me/j4n8/projekt02backend/Projekt02BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.j4n8.projekt02backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt02BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
