c5ce7175793290f7a4071bce4aa4ba848a933558
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-02-27 10:22:04 +0100

    Database config now reads values from .env file

--- src/main/java/me/j4n8/projekt02backend/DatabaseConfig.java
+++ src/main/java/me/j4n8/projekt02backend/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package me.j4n8.projekt02backend;
 
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,15 +10,29 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
 
+    @Value("${database.url}")
+    private String url;
+
+    @Value("${database.port}")
+    private int port;
+
+    @Value("${database.database}")
+    private String database;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setServerNames(new String[]{"localhost"});
-        dataSource.setPortNumbers(new int[]{5432});
-        dataSource.setDatabaseName("projekt02");
-        dataSource.setUser("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setServerNames(new String[]{url});
+        dataSource.setPortNumbers(new int[]{port});
+        dataSource.setDatabaseName(database);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
-    //TODO: Get data from .ENV file
 }
