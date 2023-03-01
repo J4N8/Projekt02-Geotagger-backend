31dda410ff1fa7cadb4d7f9cf3544fbd2dc26e0b
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-03-01 09:47:39 +0100

    Fixed location endpoint path Added ImgLocation.http for testing

--- src/main/java/me/j4n8/projekt02backend/img_location/ImgLocationController.java
+++ src/main/java/me/j4n8/projekt02backend/img_location/ImgLocationController.java
@@ -2,11 +2,13 @@ package me.j4n8.projekt02backend.img_location;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/location")
+@RestController()
+@RequestMapping("/location")
 public class ImgLocationController {
     @Autowired
     private ImgLocationService imgLocationService;
