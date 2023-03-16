967e475a086b4a51020544db41de8f57cd90dbec
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-03-16 10:11:32 +0100

    Added swagger API documentation

--- src/main/java/me/j4n8/projekt02backend/Projekt02BackendApplication.java
+++ src/main/java/me/j4n8/projekt02backend/Projekt02BackendApplication.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class Projekt02BackendApplication {
